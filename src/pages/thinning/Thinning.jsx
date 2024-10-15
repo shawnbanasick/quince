3652447bced97c8b8a11e5a2a6c503265121678c
banasick <banasick@mail.kobe-c.ac.jp>
2024-10-16 05:22:33 +0900

    more work on thinning process

--- src/pages/thinning/Thinning.jsx
+++ src/pages/thinning/Thinning.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import calculateTimeOnPage from "../../utilities/calculateTimeOnPage";
 import decodeHTML from "../../utilities/decodeHTML";
@@ -8,6 +8,7 @@ import useStore from "../../globalState/useStore";
 import PromptUnload from "../../utilities/PromptUnload";
 // import ConsentModal from "./ConsentModal";
 import parseParams from "../landing/parseParams";
+import includes from "lodash/includes";
 
 /* eslint react/prop-types: 0 */
 
@@ -18,6 +19,8 @@ const getSetProgressScore = (state) => state.setProgressScore;
 const getSetCurrentPage = (state) => state.setCurrentPage;
 const getSetDisplayNextButton = (state) => state.setDisplayNextButton;
 const getSetUrlUsercode = (state) => state.setUrlUsercode;
+
+let targetArray = [];
 
 const PostSort = () => {
   //   const ElementRef = useRef(null);
@@ -35,17 +38,70 @@ const PostSort = () => {
     localStorage.getItem("columnStatements")
   );
 
-  console.log("presortColumnStatements: ", presortColumnStatements);
-  console.log("mapObj q sort pattern: ", mapObj.qSortPattern);
-  console.log("mapObj q sort headers: ", mapObj.qSortHeaders);
-  console.log("mapObj q sort header numbers: ", mapObj.qSortHeaderNumbers);
+  // console.log("presortColumnStatements: ", presortColumnStatements);
+  // console.log("mapObj q sort pattern: ", mapObj.qSortPattern);
+  // console.log("mapObj q sort headers: ", mapObj.qSortHeaders);
+  // console.log("mapObj q sort header numbers: ", mapObj.qSortHeaderNumbers);
+
+  let headerNumbers = [...mapObj.qSortHeaderNumbers].map((item) => +item);
+  // console.log("headerNumbers: ", headerNumbers);
+
+  let minQsortValue = Math.min(...headerNumbers);
+
+  // for q sort patterns with negative values
+  if (minQsortValue < 0) {
+    let posHeaderNumbers = headerNumbers.filter((item) => item > 0);
+    let negHeaderNumbers = headerNumbers.filter((item) => item < 0);
+
+    // console.log("posHeaderNumbers: ", posHeaderNumbers);
+    // console.log("negHeaderNumbers: ", negHeaderNumbers);
+  }
 
   let sortingList = [...presortColumnStatements.statementList];
-  let posSorted = sortingList.filter((item) => item.greenChecked === true);
-  let negSorted = sortingList.filter((item) => item.pinkChecked === true);
+  sortingList.forEach((item) => {
+    item.selected = false;
+    return item;
+  });
 
-  console.log(JSON.stringify(posSorted, null, 2));
-  console.log(JSON.stringify(negSorted, null, 2));
+  let posSorted2 = sortingList.filter((item) => item.greenChecked === true);
+  let negSorted2 = sortingList.filter((item) => item.pinkChecked === true);
+
+  const [posSorted, setPosSorted] = useState(posSorted2);
+  const [negSorted, setNegSorted] = useState(negSorted2);
+
+  const handleClick = (e) => {
+    console.log("e.target.id: ", e.target.id);
+    console.log(targetArray.length);
+    if (targetArray.length > 1) {
+      targetArray.shift();
+    }
+    targetArray.push(e.target.id);
+    console.log(JSON.stringify(targetArray));
+
+    posSorted.forEach((item) => {
+      if (targetArray.includes(item.id)) {
+        item.selected = true;
+      } else {
+        item.selected = false;
+      }
+    });
+    setPosSorted([...posSorted]);
+    // console.log(JSON.stringify(posSorted));
+  };
+
+  let boxes = [...posSorted].map((item) => {
+    // create divs of posSorted items statements to add to dom
+    return (
+      <div key={item.id} onClick={handleClick}>
+        <Box id={item.id} selected={item.selected}>
+          {item.statement}
+        </Box>
+      </div>
+    );
+  });
+
+  // console.log(JSON.stringify(posSorted, null, 2));
+  // console.log(JSON.stringify(negSorted, null, 2));
 
   setDisplayNextButton(true);
 
@@ -74,9 +130,14 @@ const PostSort = () => {
       <SortTitleBar background={headerBarColor}>
         REFINE YOUR PREFERENCES
       </SortTitleBar>
-      <button>Confirm</button>
       <ContainerDiv>
-        <div>REFINE YOUR PREFERENCES</div>
+        <InstructionsDiv>
+          <Instructions>
+            Select two statements that best reflect your perspective
+          </Instructions>
+          <button>Confirm</button>
+        </InstructionsDiv>
+        <BoxesDiv>{boxes}</BoxesDiv>
       </ContainerDiv>
     </div>
   );
@@ -106,7 +167,7 @@ const ContainerDiv = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  padding: 5vw;
+  padding: 3vw;
   margin-bottom: 70px;
   padding-top: 50px;
   transition: 0.3s ease all;
@@ -120,4 +181,59 @@ const ContainerDiv = styled.div`
     margin-top: 20px;
     margin-bottom: 20px;
   }
+`;
+
+const BoxesDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding: 1vw;
+  /* border: 2px solid red; */
+`;
+
+const Box = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 170px;
+  height: 150px;
+  padding: 10px;
+  overflow: hidden;
+  margin: 10px;
+  border: 1px solid black;
+  border-radius: 10px;
+  background-color: ${(props) => (props.selected ? "lightgreen" : "white")};
+  color: black;
+  font-size: 16px;
+  font-weight: normal;
+  text-align: center;
+  cursor: pointer;
+  transition: 0.3s ease all;
+
+  &:hover {
+    background-color: lightgreen;
+  }
+`;
+
+const InstructionsDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 20px;
+  justify-content: center;
+  align-items: center;
+  padding: 1vw;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  font-size: 16px;
+  font-weight: normal;
+  text-align: center;
+  color: black;
+  /* border: 2px solid red; */
+`;
+
+const Instructions = styled.div`
+  font-size: 2.2vw;
+  font-weight: normal;
+  text-align: center;
+  color: black;
 `;
