9ca5a55380dcfb62c3b7b3c42ccee897df807348
banasick <banasick@mail.kobe-c.ac.jp>
2026-01-26 08:08:18 +0900

    vitest config update

--- vitest.config.js
+++ vitest.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   test: {
@@ -7,5 +8,5 @@ export default defineConfig({
     globals: true,
     setupFiles: "./tests/setup",
   },
-  plugins: [react()],
+  plugins: [react(), svgr()],
 });
