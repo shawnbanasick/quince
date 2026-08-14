6ff7e4527545c3bd30b98f058080e8e9c3b0a07c
banasick <banasick@mail.kobe-c.ac.jp>
2026-08-14 18:28:23 +0900

    updated xml and vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
-import { defineConfig } from "vite";
+import { defineConfig, withFilter } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react()],
+  plugins: [
+    withFilter(
+      svgr({
+        /* your options */
+      }),
+      { load: { id: /\.svg\?react$/ } },
+    ),
+    react(),
+  ],
   tests: {
     globals: true,
     environment: "jsdom",
